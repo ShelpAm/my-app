c92a8e0d4d2b52d9f02d2c976efc34836284924b
xingsirumeng <153906971+xingsirumeng@users.noreply.github.com>
2025-06-09 14:50:59 +0800

    Update Item.java

--- src/main/java/com/mycompany/app/Item.java
+++ src/main/java/com/mycompany/app/Item.java
@@ -3,7 +3,7 @@ package com.mycompany.app;
 /**
  * Item
  */
-public record Item(String name, int price, String imagePath) {
+public record Item(String name, int price, String imagePath, String discription, int isFood) {
     // public String toString() {
     //
     // }
